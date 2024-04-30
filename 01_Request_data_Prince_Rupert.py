0041040729fd61ea2ed33986c882ca9b5882ca31
francorvalan <francisco.corvalan6@gmail.com>
2024-04-29 21:39:22 -0300

    counter requests.status_code

--- 01_Request_data_Prince_Rupert.py
+++ 01_Request_data_Prince_Rupert.py
@@ -11,7 +11,7 @@ def descargar_procesar_convertir(url_list):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
     fecha_actual = datetime.now()
-
+    counter += 0
     dfs = pd.DataFrame()
     for url in url_list:
         
@@ -20,6 +20,7 @@ def descargar_procesar_convertir(url_list):
         print(respuesta)
         # Verificar si la solicitud fue exitosa
         if respuesta.status_code == 200:
+            counter += 1
             datos_json = respuesta.json()      
 
             # Procesar JSON
@@ -39,20 +40,21 @@ def descargar_procesar_convertir(url_list):
         # Unir df
         dfs = pd.concat([dfs, df])
         print(dfs.head())
+    if counter>0:
     # Convertir a GeoDataFrame
-    geometry = [Point(lon, lat) for lon, lat in zip(dfs['LON'], dfs['LAT'])]
-    gdf = gpd.GeoDataFrame(dfs, geometry=geometry, crs="EPSG:4326")
-    print(gdf)
-    # Escribir el GeoDataFrame en un archivo con nombre dependiente de la fecha
-    gdf_outputdir = "./01_Data/01_Prince_Ruport_Area/01_Shapefiles"
-    if not os.path.exists(gdf_outputdir): os.makedirs(gdf_outputdir)
-    output_file = f"{gdf_outputdir}/{fecha_actual.strftime('%Y%m%d_%H%M%S')}.shp"
-    gdf.to_file(output_file)
-    
-    geojson_outputdir = "./01_Data/01_Prince_Ruport_Area/02_Geojsons"
-    if not os.path.exists(geojson_outputdir): os.makedirs(geojson_outputdir)
-    with open(f"{geojson_outputdir}/{fecha_actual.strftime('%Y%m%d_%H%M%S')}.geojson" , 'w') as file:
-        file.write(gdf.to_json())
+        geometry = [Point(lon, lat) for lon, lat in zip(dfs['LON'], dfs['LAT'])]
+        gdf = gpd.GeoDataFrame(dfs, geometry=geometry, crs="EPSG:4326")
+        print(gdf)
+        # Escribir el GeoDataFrame en un archivo con nombre dependiente de la fecha
+        gdf_outputdir = "./01_Data/01_Prince_Ruport_Area/01_Shapefiles"
+        if not os.path.exists(gdf_outputdir): os.makedirs(gdf_outputdir)
+        output_file = f"{gdf_outputdir}/{fecha_actual.strftime('%Y%m%d_%H%M%S')}.shp"
+        gdf.to_file(output_file)
+        
+        geojson_outputdir = "./01_Data/01_Prince_Ruport_Area/02_Geojsons"
+        if not os.path.exists(geojson_outputdir): os.makedirs(geojson_outputdir)
+        with open(f"{geojson_outputdir}/{fecha_actual.strftime('%Y%m%d_%H%M%S')}.geojson" , 'w') as file:
+            file.write(gdf.to_json())
 urls = [
         'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2622/station:0',
     'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2620/station:0',
