711fc9c4fdc3da24629526ac8ad36edb6f7dea10
francorvalan <44734157+francorvalan@users.noreply.github.com>
2024-04-30 10:56:51 -0300

    Update 01_Request_data_Prince_Rupert.py

--- 01_Request_data_Prince_Rupert.py
+++ 01_Request_data_Prince_Rupert.py
@@ -68,6 +68,7 @@ def descargar_procesar_convertir(url_list):
 #     ]
 
 urls = [
+    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1130/Y:2617/station:0',
     'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1131/Y:2618/station:0',
         'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1129/Y:2618/station:0',
         'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1129/Y:2618/station:0',
