bb7488cdcf853d870ebf0d907fd432ed53c7e765
francorvalan <francisco.corvalan6@gmail.com>
2024-04-29 21:12:13 -0300

    update cron each 2 hours

--- 01_Request_data_Prince_Rupert.py
+++ 01_Request_data_Prince_Rupert.py
@@ -53,24 +53,23 @@ def descargar_procesar_convertir(url_list):
     #return gdf
 
 urls = [
+        'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2622/station:0',
+    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2620/station:0',
+    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2619/station:0',
+    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2618/station:0',
+    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2617/station:0',
+    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2621/station:0',
     'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1129/Y:2618/station:0',
     'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1129/Y:2617/station:0',
-    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2618/station:0',
-    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1130/Y:2618/station:0',
-    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1129/Y:2619/station:0',
-    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2617/station:0',
-    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1130/Y:2617/station:0',
-    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2619/station:0',
-    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1130/Y:2619/station:0',
     'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1129/Y:2621/station:0',
     'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1129/Y:2620/station:0',
-    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2621/station:0',
-    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1130/Y:2621/station:0',
+    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1129/Y:2619/station:0',
     'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1129/Y:2622/station:0',
-    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2620/station:0',
+    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1130/Y:2617/station:0',
+    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1130/Y:2619/station:0',
+    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1130/Y:2621/station:0',
     'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1130/Y:2620/station:0',
-    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1128/Y:2622/station:0',
-    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1130/Y:2622/station:0'
+    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1130/Y:2622/station:0',
+    'https://www.marinetraffic.com/getData/get_data_json_4/z:14/X:1130/Y:2618/station:0'
     ]
-
 descargar_procesar_convertir(urls)
